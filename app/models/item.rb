f4dfd4069bd7b367582b991d782659a6ff8e9084
Ubuntu <ubuntu@box-on-rails>
2017-09-27 02:19:42 +0000

    Getting Starting TDD

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :envoice
   belongs_to :product
+
+  def total
+    return self.amount * self.product.price
+  end
+
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should calculate total" do
+     apple = Product.new(price: 20)
+     item  = Item.new(product: apple, amount: 10)
+     assert_equal 200, item.total
+     assert true
+   end
+
 end
